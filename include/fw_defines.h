a44272e7187af6b5a295f0623bc0a2395dbf6a59
Al Azif <33132478+Al-Azif@users.noreply.github.com>
2020-07-09 18:56:30 -0700

    Add 6.72 Offsets

--- include/fw_defines.h
+++ include/fw_defines.h
@@ -7,6 +7,7 @@
 #define K501_XFAST_SYSCALL         0x000001C0
 #define K503_XFAST_SYSCALL         0x000001C0
 #define K505_XFAST_SYSCALL         0x000001C0
+#define K672_XFAST_SYSCALL         0x000001C0
 
 #define K405_PRISON_0              0x00F26010
 #define K455_PRISON_0              0x010399B0
@@ -14,6 +15,7 @@
 #define K501_PRISON_0              0x010986A0
 #define K503_PRISON_0              0x010986A0
 #define K505_PRISON_0              0x010986A0
+#define K672_PRISON_0              0x0113E518
 
 #define K405_ROOTVNODE             0x0206D250
 #define K455_ROOTVNODE             0x021AFA30
@@ -21,6 +23,7 @@
 #define K501_ROOTVNODE             0x022C19F0
 #define K503_ROOTVNODE             0x022C1A70
 #define K505_ROOTVNODE             0x022C1A70
+#define K672_ROOTVNODE             0x02300320
 
 #define K405_PRINTF                0x00347580
 #define K455_PRINTF                0x00017F30
@@ -28,6 +31,7 @@
 #define K501_PRINTF                0x00435C70
 #define K503_PRINTF                0x00436000
 #define K505_PRINTF                0x00436040
+#define K672_PRINTF                0x00123280
 
 #define K405_COPYIN                0x00286DF0
 #define K455_COPYIN                0x0014A890
@@ -35,6 +39,7 @@
 #define K501_COPYIN                0x001EA600
 #define K503_COPYIN                0x001EA710
 #define K505_COPYIN                0x001EA710
+#define K672_COPYIN                0x003C17A0
 
 #define K405_COPYOUT               0x00286D70
 #define K455_COPYOUT               0x0014A7B0
@@ -42,6 +47,7 @@
 #define K501_COPYOUT               0x001EA520
 #define K503_COPYOUT               0x001EA630
 #define K505_COPYOUT               0x001EA630
+#define K672_COPYOUT               0x003C16B0
 
 #define K405_MMAP_SELF_1           0x003605F0
 #define K455_MMAP_SELF_1           0x0016A5B0
@@ -49,6 +55,7 @@
 #define K501_MMAP_SELF_1           0x000117B0
 #define K503_MMAP_SELF_1           0x000117B0
 #define K505_MMAP_SELF_1           0x000117B0
+#define K672_MMAP_SELF_1           0x00233C40
 
 #define K405_MMAP_SELF_2           0x00360600
 #define K455_MMAP_SELF_2           0x0016A5C0
@@ -56,6 +63,7 @@
 #define K501_MMAP_SELF_2           0x000117C0
 #define K503_MMAP_SELF_2           0x000117C0
 #define K505_MMAP_SELF_2           0x000117C0
+#define K672_MMAP_SELF_2           0x00233C50
 
 #define K405_MMAP_SELF_3           0x0031EE37
 #define K455_MMAP_SELF_3           0x00143BE7
@@ -63,5 +71,6 @@
 #define K501_MMAP_SELF_3           0x0013EF2F
 #define K503_MMAP_SELF_3           0x0013F03F
 #define K505_MMAP_SELF_3           0x0013F03F
+#define K672_MMAP_SELF_3           0x000AD2E4
 
 #endif
