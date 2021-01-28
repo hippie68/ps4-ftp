f460bade64d8c5293b5370a2346baa49dcaf2b39
Al-Azif <alazif@miskatonic.io>
2021-01-27 17:48:59 -0800

    Change "socket" to "debug"

--- source/main.c
+++ source/main.c
@@ -150,7 +150,7 @@ int _main(struct thread *td) {
   printf_notification("Shutting down FTP server...");
 
 #ifdef DEBUG_SOCKET
-  printf_socket("\nClosing socket...\n\n");
+  printf_debug("Closing socket...\n");
   SckClose(DEBUG_SOCK);
 #endif
 
